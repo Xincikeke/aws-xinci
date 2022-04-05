b8acb98a7f6bc67c5115c33444ffafdf168dfd3f
Lee Xinci <88148055+Xincikeke@users.noreply.github.com>
2022-04-06 03:20:21 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -37,6 +37,7 @@ def editEmpPage():
 
 @app.route("/addEmp", methods=['POST'])
 def AddEmp():
+    
     first_name = request.form['firstname']
     last_name = request.form['lastname']
     pri_skill = request.form['pri_skill']
