bd7e0d5812c150001d9b150594b0b359abb986d0
Lee Xinci <88148055+Xincikeke@users.noreply.github.com>
2022-04-05 14:11:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cwb5ospopmyd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "leexinci-employee"
 customregion = "us-east-1"
